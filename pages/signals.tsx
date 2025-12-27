8f97890c3e36e2fa91342cfb3d12ff87c5465292
Bohdan Maliar <bohdan6992@users.noreply.github.com>
2025-12-27 15:52:52 -0500

    TILES COMMIT

--- pages/signals.tsx
+++ pages/signals.tsx
@@ -3,11 +3,21 @@ import LiveStrategyTiles from "@/components/signals/LiveStrategyTiles";
 
 export default function SignalsPage() {
   return (
-    <main className="page space-y-6">
-      <header className="flex items-center justify-between">
-      </header>
+    <main className="relative min-h-screen">
+      {/* 
+          Цей блок перекриває топбар. 
+          mt-[-100px] піднімає його вгору, h-[100px] створює простір, 
+          а z-50 гарантує, що він буде над навігацією.
+      */}
+      <div className="absolute top-[-100px] left-0 right-0 h-[120px] bg-[#030303] z-[40] pointer-events-none" />
 
-      <LiveStrategyTiles />
+      <div className="page space-y-6 relative z-10 pt-4">
+        <header className="flex items-center justify-between">
+          {/* Тут можна додати заголовок сторінки, якщо потрібно */}
+        </header>
+
+        <LiveStrategyTiles />
+      </div>
     </main>
   );
 }
